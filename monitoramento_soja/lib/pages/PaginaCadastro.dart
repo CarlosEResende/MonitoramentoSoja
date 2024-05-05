851f64f3cd204871bbc6a2866469d78dc936abc7
Carlos Eduardo <carlozresende@gmail.com>
2024-05-04 21:32:44 -0300

    Atualizações

--- monitoramento_soja/lib/pages/PaginaCadastro.dart
+++ monitoramento_soja/lib/pages/PaginaCadastro.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+
+class Cadastro extends StatelessWidget {
+  const Cadastro({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cadastro'),
+      ),
+      body: Center(
+        child: Text('Página de Cadastro'),
+      ),
+    );
+  }
+}
