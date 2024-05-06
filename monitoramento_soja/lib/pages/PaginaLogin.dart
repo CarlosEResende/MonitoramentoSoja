0464085e3367d9d9cf419d93eb0b467dab283c25
Carlos Eduardo <carlozresende@gmail.com>
2024-05-06 04:59:57 -0300

    Começo do Login

--- monitoramento_soja/lib/pages/PaginaLogin.dart
+++ monitoramento_soja/lib/pages/PaginaLogin.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:monitoramento_soja/pages/pagina_inicial.dart';
 
 class Login extends StatelessWidget {
   const Login({Key? key}) : super(key: key);
@@ -6,11 +7,73 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: Center(
-        child: Text('Página de Login'),
+      body: Stack(
+        children: [
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color.fromARGB(255, 255, 255, 255)],
+              ),
+            ),
+          ),
+          Positioned(
+            top: 90,
+            left: 70,
+            child: Image.asset(
+              'lib/img/logos.png',
+              height: 200,
+              width: 300,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Positioned.fill(
+            top: 350.0,
+            child: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(60),
+                  topRight: Radius.circular(60),
+                ),
+                color: Color(0xFFB1D3A9),
+              ),
+              child: Stack(
+                children: [
+                  Positioned(
+                    top: 30,
+                    left: 30,
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => PaginaInicial()),
+                        );
+                      },
+                      child: Image.asset(
+                        'lib/img/back.png',
+                        height: 40,
+                        width: 40,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30, left: 175),
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                        color: Color(0xFF19480D),
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
